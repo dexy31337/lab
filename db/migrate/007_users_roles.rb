9a848fb757f36cceee64ece2f082008eb06c00d8
(no author) <(no author)@04f79dd5-9327-0410-9ae1-82433a0e7d43>
2007-04-01 20:23:39 +0000

    RBAC!
    git-svn-id: svn://192.168.157.1/repos/lab/trunk@62 04f79dd5-9327-0410-9ae1-82433a0e7d43

--- db/migrate/007_users_roles.rb
+++ db/migrate/007_users_roles.rb
@@ -1,13 +1,7 @@
 class UsersRoles < ActiveRecord::Migration
   def self.up
-    create_table "users", :force => true do |t|
-      t.column "login", :string, :limit => 40
-      t.column "password", :string, :limit => 40 
-    end
-    #User.create(:login => 'admin',:password => 'password',:password_confirmation => 'password')
   end
 
   def self.down
-    drop_table :users
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -2,21 +2,21 @@
 # migrations feature of ActiveRecord to incrementally modify your database, and
 # then regenerate this schema definition.
 
-ActiveRecord::Schema.define(:version => 8) do
+ActiveRecord::Schema.define(:version => 9) do
 
   create_table "devices", :force => true do |t|
-    t.column "name", :string, :default => "", :null => false
-    t.column "partnumber", :string, :default => "", :null => false
-    t.column "serialnumber", :string
-    t.column "labnumber", :string, :default => "", :null => false
-    t.column "device_id", :integer
-    t.column "vendor_id", :integer, :default => 0, :null => false
-    t.column "location", :string
-    t.column "box_location", :string
-    t.column "reservation_id", :integer
-    t.column "comment", :text
+    t.column "name",                :string,  :default => "", :null => false
+    t.column "partnumber",          :string,  :default => "", :null => false
+    t.column "serialnumber",        :string
+    t.column "labnumber",           :string,  :default => "", :null => false
+    t.column "device_id",           :integer
+    t.column "vendor_id",           :integer, :default => 0,  :null => false
+    t.column "location",            :string
+    t.column "box_location",        :string
+    t.column "reservation_id",      :integer
+    t.column "comment",             :text
     t.column "serving_property_id", :integer
-    t.column "modular", :boolean
+    t.column "modular",             :boolean
   end
 
   add_index "devices", ["device_id"], :name => "fk_device_device"
@@ -24,24 +24,56 @@ ActiveRecord::Schema.define(:version => 8) do
   add_index "devices", ["reservation_id"], :name => "fk_device_reservation"
   add_index "devices", ["serving_property_id"], :name => "fk_device_serving_property"
 
+  create_table "engine_schema_info", :id => false, :force => true do |t|
+    t.column "engine_name", :string
+    t.column "version",     :integer
+  end
+
   create_table "engineers", :force => true do |t|
-    t.column "first", :string
-    t.column "last", :string
-    t.column "middle", :string
+    t.column "first",      :string
+    t.column "last",       :string
+    t.column "middle",     :string
     t.column "work_phone", :string
     t.column "cell_phone", :string
-    t.column "email", :string
+    t.column "email",      :string
   end
+
+  create_table "groups", :force => true do |t|
+    t.column "created_at", :datetime,                                :null => false
+    t.column "updated_at", :datetime,                                :null => false
+    t.column "title",      :string,   :limit => 200, :default => "", :null => false
+    t.column "parent_id",  :integer,  :limit => 10
+  end
+
+  add_index "groups", ["parent_id"], :name => "groups_parent_id_index"
+
+  create_table "groups_roles", :id => false, :force => true do |t|
+    t.column "group_id",   :integer,  :limit => 10, :default => 0, :null => false
+    t.column "role_id",    :integer,  :limit => 10, :default => 0, :null => false
+    t.column "created_at", :datetime,                              :null => false
+  end
+
+  add_index "groups_roles", ["group_id", "role_id"], :name => "groups_roles_all_index", :unique => true
+  add_index "groups_roles", ["role_id"], :name => "role_id"
+
+  create_table "groups_users", :id => false, :force => true do |t|
+    t.column "group_id",   :integer,  :limit => 10, :default => 0, :null => false
+    t.column "user_id",    :integer,  :limit => 10, :default => 0, :null => false
+    t.column "created_at", :datetime,                              :null => false
+  end
+
+  add_index "groups_users", ["group_id", "user_id"], :name => "groups_users_all_index", :unique => true
+  add_index "groups_users", ["user_id"], :name => "user_id"
 
   create_table "interface_types", :force => true do |t|
     t.column "name", :string, :default => "", :null => false
   end
 
   create_table "interfaces", :force => true do |t|
-    t.column "name", :string, :default => "", :null => false
-    t.column "interface_type_id", :integer, :default => 0, :null => false
-    t.column "device_id", :integer, :default => 0, :null => false
-    t.column "interface_id", :integer
+    t.column "name",              :string,  :default => "", :null => false
+    t.column "interface_type_id", :integer, :default => 0,  :null => false
+    t.column "device_id",         :integer, :default => 0,  :null => false
+    t.column "interface_id",      :integer
   end
 
   add_index "interfaces", ["interface_type_id"], :name => "fk_interface_interface_type"
@@ -49,29 +81,85 @@ ActiveRecord::Schema.define(:version => 8) do
   add_index "interfaces", ["interface_id"], :name => "fk_interface_interface"
 
   create_table "reservations", :force => true do |t|
-    t.column "start", :date
-    t.column "end", :date
-    t.column "comment", :text
+    t.column "start",       :date
+    t.column "end",         :date
+    t.column "comment",     :text
     t.column "engineer_id", :integer
   end
 
   add_index "reservations", ["engineer_id"], :name => "fk_erservation_engineer"
 
-  create_table "serving_properties", :force => true do |t|
-    t.column "cpu_count", :integer
-    t.column "cpu_type", :string
-    t.column "ram", :integer
-    t.column "disks_total", :integer
-    t.column "disks_layout", :string
-    t.column "os", :string
-    t.column "admin_login", :string
-    t.column "admin_pass", :string
+  create_table "roles", :force => true do |t|
+    t.column "created_at", :datetime,                                :null => false
+    t.column "updated_at", :datetime,                                :null => false
+    t.column "title",      :string,   :limit => 100, :default => "", :null => false
+    t.column "parent_id",  :integer,  :limit => 10
   end
 
-  create_table "users", :force => true do |t|
-    t.column "login", :string, :limit => 40
-    t.column "password", :string, :limit => 40
+  add_index "roles", ["parent_id"], :name => "roles_parent_id_index"
+
+  create_table "roles_static_permissions", :id => false, :force => true do |t|
+    t.column "role_id",              :integer,  :limit => 10, :default => 0, :null => false
+    t.column "static_permission_id", :integer,  :limit => 10, :default => 0, :null => false
+    t.column "created_at",           :datetime,                              :null => false
   end
+
+  add_index "roles_static_permissions", ["static_permission_id", "role_id"], :name => "roles_static_permissions_all_index", :unique => true
+  add_index "roles_static_permissions", ["role_id"], :name => "role_id"
+
+  create_table "roles_users", :id => false, :force => true do |t|
+    t.column "user_id",    :integer,  :limit => 10, :default => 0, :null => false
+    t.column "role_id",    :integer,  :limit => 10, :default => 0, :null => false
+    t.column "created_at", :datetime,                              :null => false
+  end
+
+  add_index "roles_users", ["user_id", "role_id"], :name => "roles_users_all_index", :unique => true
+  add_index "roles_users", ["role_id"], :name => "role_id"
+
+  create_table "serving_properties", :force => true do |t|
+    t.column "cpu_count",    :integer
+    t.column "cpu_type",     :string
+    t.column "ram",          :integer
+    t.column "disks_total",  :integer
+    t.column "disks_layout", :string
+    t.column "os",           :string
+    t.column "admin_login",  :string
+    t.column "admin_pass",   :string
+  end
+
+  create_table "static_permissions", :force => true do |t|
+    t.column "title",      :string,   :limit => 200, :default => "", :null => false
+    t.column "created_at", :datetime,                                :null => false
+    t.column "updated_at", :datetime,                                :null => false
+  end
+
+  add_index "static_permissions", ["title"], :name => "static_permissions_title_index", :unique => true
+
+  create_table "user_registrations", :force => true do |t|
+    t.column "user_id",    :integer,  :limit => 10, :default => 0,  :null => false
+    t.column "token",      :text,                   :default => "", :null => false
+    t.column "created_at", :datetime,                               :null => false
+    t.column "expires_at", :datetime,                               :null => false
+  end
+
+  add_index "user_registrations", ["user_id"], :name => "user_registrations_user_id_index", :unique => true
+  add_index "user_registrations", ["expires_at"], :name => "user_registrations_expires_at_index"
+
+  create_table "users", :force => true do |t|
+    t.column "created_at",          :datetime,                                          :null => false
+    t.column "updated_at",          :datetime,                                          :null => false
+    t.column "last_logged_in_at",   :datetime,                                          :null => false
+    t.column "login_failure_count", :integer,  :limit => 10,  :default => 0,            :null => false
+    t.column "login",               :string,   :limit => 100, :default => "",           :null => false
+    t.column "email",               :string,   :limit => 200, :default => "",           :null => false
+    t.column "password",            :string,   :limit => 100, :default => "",           :null => false
+    t.column "password_hash_type",  :string,   :limit => 20,  :default => "",           :null => false
+    t.column "password_salt",       :string,   :limit => 10,  :default => "1234512345", :null => false
+    t.column "state",               :integer,  :limit => 10,  :default => 1,            :null => false
+  end
+
+  add_index "users", ["login"], :name => "users_login_index", :unique => true
+  add_index "users", ["password"], :name => "users_password_index"
 
   create_table "vendors", :force => true do |t|
     t.column "name", :string, :default => "", :null => false
