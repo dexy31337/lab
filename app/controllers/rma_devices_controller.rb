b4b258db66160339d51b7a9183cbe6e16ba53e5a
(no author) <(no author)@04f79dd5-9327-0410-9ae1-82433a0e7d43>
2009-04-30 12:38:53 +0000

    добавил функцию сохранения выбранного партномера девайса в контролерре (как в резервации)
    git-svn-id: svn://192.168.157.1/repos/lab/trunk@176 04f79dd5-9327-0410-9ae1-82433a0e7d43

--- app/controllers/rma_devices_controller.rb
+++ app/controllers/rma_devices_controller.rb
@@ -14,13 +14,25 @@ class RmaDevicesController < ApplicationController
   end
 
 
+  def rma_log (device,rma_device_id)
+    device.rma_device_id = rma_device_id
+    device.save
+    Device.find(:all,:conditions => ['device_id = ?',device[:id]]).collect.each do |dev|
+      rma_log (dev,rma_device_id)
+    end
+  end
+
   def create
     @rma_devices = RmaDevice.new(params[:rma_devices])
-
-
     @rma_devices.save
-
-      redirect_to :action => 'list'
+   Device.find(:all).each do |dev|
+      if(params['device_'+dev.id.to_s] != nil )
+        checked = params['device_'+dev.id.to_s]['checked']
+          if(checked != nil && checked.length > 0)
+#            dev.reservation_id = @reservation.id
+#            dev.save
+             rma_log(dev,@rma_devices[:id])
+     redirect_to :action => 'list'
 
   end
 
